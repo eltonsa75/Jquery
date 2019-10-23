fd345de95fc3940459f3025bed85a3bf6aa8f077
Elton Andrade <eltonsa75@hotmail.com>
2019-10-23 00:02:37 -0300

    Atualizando o JS

--- js/03.js
+++ js/03.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
     $("*").css("color", "#0000ff");
-    $("#titulo").css("color", "green");    
+    // Div
+    $("#titulo").css("color", "green");  
+    // Href  
     $("a").css("color", "red");
+    // Class
     $(".outro").css("color", "black");
     $("h1, a").css("background-color", "yellow");
 });
